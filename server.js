f2e7f9da6846bbeddb94a97eb59890061cba9639
shashank <sauravshashank2001@gmail.com>
2023-01-09 02:27:51 +0530

    new

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ process.on("uncaughtException", (err) => {
 
 // Config
 
-  require("dotenv").config({ path: "backend/config/config.env" });
+  require("dotenv").config({ path: "config/config.env" });
 
 
 // Connecting to database
